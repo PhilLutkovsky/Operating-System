2b17f0c071f29750913a5f3a5313b6528c62cc70
PhilLutkovsky <7plutkovsky@stpatrick.org>
2016-10-13 13:04:28 -0500

    Finished and run "Operating System" succesfully

--- AppObjectPhil.js
+++ AppObjectPhil.js
@@ -9,30 +9,42 @@ function App(n,m,s){
     this.open = function(){
       this.state = "active";
 
-
     };
     //Declare the instance function sleep here.
     this.sleep = function(){
      if(this.state == "active"){
        this.state = "sleep";
-       this.memory = this.memory/2
+       this.memory = this.memory/2;
      }
     };
     //Declare the instance function active here.
     this.active = function(){
-      if(this.state == )
+      if(this.state == "sleep"){
+        this.state = "active";
+        this.memory = this.memory*2;
+      }
+    };
 
 
     //Declare the instance function close here.
-
-
-
-
+    this.close = function(){
+      if(this.state != null){
+        this.state = null;
+        this.memory = 0;
+      }
+    };
 }
 
 //Use the constructor to create 4 App objects below.
 
+var internet = new App("chrome",200,null);
+var music = new App("iTunes",100,null);
+var facebook = new App("social",50,null);
+var calcultor = new App("program",25,null);
 
 
 //Declare an array named appList, and place all 4 of your apps in that array.
 //The order does not matter.
+
+var appList = [internet,music,facebook,calcultor];
+console.log(appList)
